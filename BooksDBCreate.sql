dbb19b1edc344beeaaf6211a383766abd205fa5f
lenaonisch <elenaonishhenk0@gmail.com>
2018-04-02 23:35:45 +0300

    .

---
+++ BooksDBCreate.sql
@@ -0,0 +1,51 @@
+﻿
+create table Books (
+   ID             	bigint               identity,
+   Name             varchar(50)          not null,
+   PublishedIn      date          		 null,
+   constraint PK_Books primary key (ID)
+)
+go
+
+create table Authors (
+   ID				bigint               identity,
+   Name             varchar(50)          not null,
+   Surname          varchar(50)          not null,
+   Country          varchar(50)          null,
+   constraint PK_Author primary key (ID)
+)
+go
+
+
+create table BooksAuthors (
+   BookID           bigint               not null,
+   AuthorID         bigint               not null,
+   constraint PK_BookAuth primary key (BookID, AuthorID),
+   constraint FK_Books foreign key (BookID)
+      references Books (ID),
+   constraint FK_Authors foreign key (AuthorID)
+      references Authors (ID)
+)
+go
+
+INSERT INTO Authors (Name, Surname, Country) VALUES ('Nil', 'Gaiman', 'England')
+INSERT INTO Authors (Name, Surname, Country) VALUES ('Terry', 'Pratchett', 'England')
+INSERT INTO Authors (Name, Surname, Country) VALUES ('Micle', 'Rivs', 'USA')
+
+INSERT INTO Books 	(Name, PublishedIn) VALUES ('Good omens', '2010')
+INSERT INTO BooksAuthors (BookID, AuthorID) VALUES (1, 1)
+INSERT INTO BooksAuthors (BookID, AuthorID) VALUES (1, 2)
+
+INSERT INTO Books 	(Name, PublishedIn) VALUES ('American gods', '2001')
+INSERT INTO BooksAuthors (BookID, AuthorID) VALUES (2, 1)
+
+INSERT INTO Books 	(Name, PublishedIn) VALUES ('Coralina', '2002')
+INSERT INTO BooksAuthors (BookID, AuthorID) VALUES (3, 1)
+
+INSERT INTO Books 	(Name, PublishedIn) VALUES ('Small gods', '1992')
+INSERT INTO BooksAuthors (BookID, AuthorID) VALUES (4, 2)
+
+INSERT INTO Books 	(Name, PublishedIn) VALUES ('Interworld', '2008')
+INSERT INTO BooksAuthors (BookID, AuthorID) VALUES (5, 1)
+INSERT INTO BooksAuthors (BookID, AuthorID) VALUES (5, 3)
+
--- ORM/Program.cs
+++ ORM/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Dapper;
 
 namespace ORM
 {
@@ -10,6 +9,88 @@ namespace ORM
     {
         static void Main(string[] args)
         {
+            EFTest();
+            Console.ReadLine();
+
+            DapperTest();
+            Console.ReadLine();
+        }
+
+        private static void DapperTest()
+        {
+            using (var sqlConnection = new System.Data.SqlClient.SqlConnection(@"data source=(LocalDB)\MSSQLLocalDB;attachdbfilename=|DataDirectory|\DBBooks.mdf;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework"))
+            {
+                string sqlAllAuthors =
+                    @"SELECT A.Name, A.Surname, A.Country, BookCount from Authors as A,
+	                  (select BA.AuthorID, COUNT(BA.BookID) as BookCount 
+                        FROM BooksAuthors as BA
+	                    GROUP BY BA.AuthorID) as T
+                      WHERE T.AuthorID=A.ID";
+                PrintAuthors("Dapper", sqlConnection.Query(sqlAllAuthors));
+
+                Console.WriteLine("\nEnter surname to search for books:");
+                string surname = Console.ReadLine();
+                string sqlBooks =
+                    @"SELECT Books.Name as Book, Authors.Surname, Authors.Name FROM BooksAuthors, Books, Authors 
+                        WHERE BooksAuthors.BookID=Books.ID AND Authors.ID=BooksAuthors.AuthorID AND
+	                    BooksAuthors.AuthorID in (SELECT ID FROM Authors WHERE Surname LIKE'%" + surname + "%')";
+
+                PrintBooks("EF", surname, sqlConnection.Query(typeof(string), sqlBooks));
+            }
+        }
+
+        private static void EFTest()
+        {
+            using (DBBooksEntities db = new DBBooksEntities())
+            {
+
+                PrintAuthors("EF",
+                            from a in db.Authors
+                            select new
+                            {
+                                Name = a.Name,
+                                Surname = a.Surname,
+                                Country = a.Country,
+                                BookCount = a.Books.Count
+                            });
+
+                Console.WriteLine("\nEnter surname to search for books:");
+                string surname = Console.ReadLine();
+                IEnumerable<string> res = from b in db.Books.Where(b => b.Authors.Count(a => a.Surname.ToLower().Contains(surname.ToLower())) > 0)
+                                          select b.Name;
+                PrintBooks("EF", surname, res);
+            }
+        }
+
+        private static void PrintAuthors(string method, IEnumerable<dynamic> authors)
+        {
+            Console.WriteLine(" {0}. ALL AUTHORS:", method);
+            Console.WriteLine("Name\t\tSurname\t\tCountry\t\tWriten books");
+            Console.WriteLine("------------------------------------------------");
+            foreach (var t in authors)
+            {
+                Console.WriteLine("{0}\t\t{1,-15}{2}\t\t{3}", t.Name, t.Surname, t.Country, t.BookCount);
+            }
+        }
+
+        private static void PrintBooks(string method, string surname, IEnumerable<dynamic> books)
+        {
+            
+            Console.WriteLine("  {1}. *{0}*'s books are:", surname, method);
+            Console.WriteLine("------------------------------------------------");
+
+            
+            if (books.Count() > 0)
+            {
+                foreach (string book in books)
+                {
+                    Console.WriteLine(book);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No books found!");
+            }
         }
     }
 }
